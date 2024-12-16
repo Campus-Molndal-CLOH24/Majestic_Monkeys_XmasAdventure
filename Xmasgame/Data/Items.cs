ed761acf12698e82f4269e18380f71c6386b0d0c
Yotaka k. <88yotkha@gafe.molndal.se>
2024-12-16 23:46:22 +0100

    feature : add data and logic folder

---
+++ Xmasgame/Data/Items.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xmasgame.Data
+{
+    public class Items
+    {
+        public int ItemID { get; set; }
+        public string ItemName { get; set; }
+        public string ItemDescription { get; set; } = string.Empty;
+    }
+}
---
+++ Xmasgame/Data/Rooms.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xmasgame.Data
+{
+    public class Rooms
+    {
+        public int RoomsId {get; set; }
+        public string RoomsName { get; set; }
+        public string RoomsDescription { get; set; } = string.Empty;
+    }
+}
