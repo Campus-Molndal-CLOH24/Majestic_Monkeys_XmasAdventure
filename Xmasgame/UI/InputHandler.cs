f91f2f1568ec570815142a838fd287c8e7c76f25
Yotaka k. <88yotkha@gafe.molndal.se>
2024-12-16 23:29:16 +0100

    feature : add method to handle with rooms choices, it will become more later

--- Xmasgame/UI/InputHandler.cs
+++ Xmasgame/UI/InputHandler.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace Xmasgame.UI
 {
-    internal class InputHandler
+    public class InputHandler
     {
+        public static int GetRoomchoice(string[] rooms)
+        {
+            while (true)
+            {
+                // display only list where player want to go 
+                Console.WriteLine("\nChoose a room to go : ");
+                for (int i = 0; i < rooms.Length; i++)
+                {
+                    Console.WriteLine($"{i + 1}. {rooms[i]}");
+                }
+                //handle with choice i tried without before it was a forever run all the times >0<
+                //so we need it to handle with choice which we use index to handle it. 
+                Console.Write("Enter your choice: ");
+                if (int.TryParse(Console.ReadLine(), out int roomChoice) && roomChoice >= 1 && roomChoice <= rooms.Length)
+                {
+                    Console.WriteLine($"You chose option {roomChoice}");
+                    return roomChoice - 1; // Convert to zero-based index
+                }
+                Console.WriteLine("Invalid input. Please try again.");
+            }
+        }
     }
 }
