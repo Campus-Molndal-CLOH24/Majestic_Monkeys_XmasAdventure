002d6b78678e5aedb86ca0c34499f37db1ec23af
Yotaka k. <88yotkha@gafe.molndal.se>
2024-12-16 23:32:13 +0100

    refactor : delected some code that is unnecessary

--- Xmasgame/Program.cs
+++ Xmasgame/Program.cs
@@ -9,5 +9,13 @@ public static class Program
     public static void Main(string[] args)
     {
         MainMenu.HandleMainMenu();
+       
+
+        int magicBallsFound = 0;
+        int totalMagicBalls = 5;
+        int lives = 3;
+        int attemptsLeft = 5;
+
+        
     }
 }
